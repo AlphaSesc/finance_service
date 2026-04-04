25e66f9625fbaf0601ae61073a933f83a7bc358a
AlphaSesc <anuz.g12345@gmail.com>
2026-04-04 22:51:01 +0545

    feat: Added Invoiceresponse

---
+++ src/main/java/com/example/finance_service/dto/InvoiceResponse.java
@@ -0,0 +1,22 @@
+package com.example.finance_service.dto;
+
+import com.example.finance_service.entity.InvoiceStatus;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InvoiceResponse {
+
+    private Long id;
+    private String studentId;
+    private String courseCode;
+    private BigDecimal amount;
+    private InvoiceStatus status;
+    private LocalDateTime createdAt;
+}
